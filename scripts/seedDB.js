35b59e5d48e50e72fe2510b6047e68913665c68c
Arvin John Bautista <arvinyoung01@gmail.com>
2021-05-21 17:45:42 -0700

    script seed tested!

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -2,23 +2,17 @@ const mongoose = require("mongoose");
 const db = require("../models");
 
 // This file empties the Books collection and inserts the books below
-
 db.connect();
 
 const bookSeed = [
   {
     title: "The Dead Zone",
-    author: "Stephen King",
-    synopsis:
+    authors: ["Stephen King"],
+    description:
       'A number-one national best seller about a man who wakes up from a five-year coma able to see people\'s futures and the terrible fate awaiting mankind in The Dead Zone - a "compulsive page-turner" (The Atlanta Journal-Constitution). Johnny Smith awakens from a five-year coma after his car accident and discovers that he can see people\'s futures and pasts when he touches them. Many consider his talent a gift; Johnny feels cursed. His fiancée married another man during his coma, and people clamor for him to solve their problems. When Johnny has a disturbing vision after he shakes the hand of an ambitious and amoral politician, he must decide if he should take drastic action to change the future. The Dead Zone is a "faultlessly paced...continuously engrossing" (Los Angeles Times) novel of second sight.',
-    date: new Date(Date.now()),
-  },
-  {
-    authors: ["William Golding"],
-    title: "Lord of the Flies",
-    synopsis:
-      "The tale of a party of shipwrecked schoolboys, marooned on a coral island, who at first enjoy the freedom of the situation but soon divide into fearsome gangs which turn the paradise island into a nightmare of panic and death.",
-    date: new Date(Date.now()),
+    image:
+      "http://books.google.com/books/content?id=O53jCwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+    link: "http://books.google.com/books?id=O53jCwAAQBAJ&dq=The+Dead+Zone&hl=&source=gbs_api",
   },
   {
     title: "The Hunger Games",
