804cafe8d182084a5cd913d4abd00721c28921a0
Arvin John Bautista <arvinyoung01@gmail.com>
2021-05-24 18:36:45 -0700

    Delete error fixes

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,39 +1,43 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import BookList from "../components/BookList";
 import API from "../utils/API";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function Saved() {
-    const [state, setState] = useState({
-        books: []
-    });
+  const [state, setState] = useState({
+    books: [],
+  });
 
-    function getSavedBooks() {
-        API.getBooksAPI().then(res => setState({...state, books: res.data}));
-    };
+  function getSavedBooks() {
+    API.getBooksAPI().then((res) => setState({ ...state, books: res.data }));
+  }
 
-    useEffect(getSavedBooks,[state]);
+  useEffect(getSavedBooks, [state]);
 
-    function deleteToast(title) {
-        return toast.error(`Book deleted successfully: ${title}`);
-    }
+  function deleteToast(title) {
+    return toast.error(`Book titled ${title} deleted!`);
+  }
 
-    async function handleDelete(e) {
-        e.preventDefault();
-        const id = e.target.value;
-        const book = state.books.filter(book => book.id === id);
-        await API.deleteBookAPI(id);
-        deleteToast(book[0].title);
-        getSavedBooks();
-    }
+  async function handleDelete(e) {
+    e.preventDefault();
+    const id = e.target.value;
+    const book = state.books.filter((book) => book.id === id);
+    await API.deleteBookAPI(id);
+    deleteToast(book[0].title);
+    getSavedBooks();
+  }
 
-    return (
-        <>
-            <BookList books={state.books} handleDelete={handleDelete} />
-            <ToastContainer position="bottom-right" autoClose={5000} hideProgressBar />
-        </>
-    )
-};
+  return (
+    <>
+      <BookList books={state.books} handleDelete={handleDelete} />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar
+      />
+    </>
+  );
+}
 
 export default Saved;
